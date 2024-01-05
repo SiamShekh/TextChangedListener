45f134a8971b7d9fc3f03aae33f8a51ab9942e32
PlanqSiam <planqnetwork@gmail.com>
2024-01-05 19:09:41 +0600

    1.0

---
+++ app/src/main/java/com/siam/edittextonchanged/MainActivity.java
@@ -0,0 +1,47 @@
+package com.siam.edittextonchanged;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView hello;
+    EditText editTextText;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hello = findViewById(R.id.hello);
+        editTextText = findViewById(R.id.editTextText);
+
+        editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().equals("SiamShekh")){
+                    hello.setVisibility(View.VISIBLE);
+                }else {
+                    hello.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
